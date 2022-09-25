610180bf321fa8472d94b715e536f2f622cb1d3d
Naatty123 <nattynengeda@gmail.com>
2022-09-25 07:27:46 +0300

    Second Commit

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,21 @@
 <script>
   import './app.css'
+  var count = 0;
+  function incrementCount(){
+    count+=1;
+  }
+  
+  $: if(count >=10 ){
+    console.log('THe element is hight')
+  }
 
-  let name = 'world'
+  $: console.log('hello natty')
+
 </script>
-<h1 class="text-">Hello {name}</h1>
+
+
+<h1 class="text-5xl font-bold text-blue-500">Hello {name}</h1>
+<button
+class="py-3 px-4 rounded-xl bg-blue-500 text-white text-xl"
+on:click={incrementCount}>{count}</button>
+<style></style>
