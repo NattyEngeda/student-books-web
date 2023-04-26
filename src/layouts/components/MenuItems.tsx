c6db9c1344e7fdcde129111b4b4e9dcb3e9781de
NattyEngeda <nattynengeda@gmail.com>
2023-04-26 06:40:37 +0300

    Updated React Commit

--- src/layouts/components/MenuItems.tsx
+++ src/layouts/components/MenuItems.tsx
@@ -3,11 +3,12 @@ import { NavLink } from "@mantine/core";
 export default function MenuItems(props: any) {
   return (
     <NavLink
-      top={props.link}
+      to={props.link}
       className={({ isActive }) =>
-        isActive ? "activeHeader" : "inactiveHeader"
+        isActive ? "activeLink" : "inactiveLink"
       }>
-      <div className="flex justify-end gap-1 items-end" >
+      <div className='flex justify-end gap-1 items-end '>
+        {/* {checkIcon(pageName)} */}
         <span>{props.name}</span>
       </div>
     </NavLink>
